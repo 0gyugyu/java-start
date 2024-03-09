d2453c4624f8d414fc92fe463544949c8bed2d21
0gyugyu <ease-hidings-0h@icloud.com>
2024-03-09 22:55:02 +0900

    240309 자바 기초 공부

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;//변수선언
+        a = 10; //변수 초기화 a(10)
+        System.out.println(a); //10 출력
+        a = 50; //변수 값 변경 a(10 -> 50), 기존은 제거
+        System.out.println(a);
+    }
+}
