745315a96612d699c7a400c64c2e17731d689db2
0gyugyu <dudrbrla97@gmail.com>
2024-03-12 11:41:59 +0900

    Java

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100; // 정수. 문자는 담을 수 없다..
+        double b = 10.5; //실수
+        boolean c = true; //불리언(boolean) true, false 입력 가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+        // int z = "백원"; // 잘못된 데이터 타입. ""로 가두면 문자열
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
